76354e1e51cd56a2adc810ac5e385f1a7601a915
pontura <pontura@gmail.com>
2019-02-22 13:26:32 -0300

    first

---
+++ src/VRVaccineAdults/Assets/PersistentData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentData : MonoBehaviour {
+
+	public bool DEBBUGER;
+	public int num_of_vaccines;
+	public AudiosManager audios;
+	public bool langSelected;
+	public languages lang;
+	public ServerLogin serverLogin;
+	public enum languages
+	{
+		EN,
+		ES,
+		PO,
+		FR,
+		AR
+	}
+	public static PersistentData Instance { get; protected set; }
+
+	void Awake()
+	{
+		Instance = this;
+		print ("______ arranca");
+		DontDestroyOnLoad (this.gameObject);
+		audios = GetComponent<AudiosManager> ();
+		serverLogin = GetComponent<ServerLogin> ();
+		serverLogin.Init ();
+	}
+	void OnDestroy()
+	{
+		print ("se rompe");
+	}
+
+}
